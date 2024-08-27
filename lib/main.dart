d489eac7679e30c06e5e5e8976c4fa8a1ec18330
Myint Myat Soe <max@max-mac.local>
2024-08-27 13:49:09 +0630

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_one/figma_home_page.dart';
-import 'package:flutter_test_one/login_page.dart';
-import 'package:flutter_test_one/home_page.dart';
-import 'package:flutter_test_one/signup_page.dart';
-import 'package:flutter_test_one/test_layout.dart';
-import 'package:flutter_test_one/test_layout_2.dart';
 
 void main(){
   runApp(
@@ -13,8 +8,8 @@ void main(){
       theme: ThemeData(
         useMaterial3: false,
       ),
-      // home: const FigmaHomePage(),
-      home: const HomePage(),
+      home: const FigmaHomePage(),
+      // home: const HomePage(),
     ),
   );
 }
